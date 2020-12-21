09cbb872eea6e2dbdc77bc0f84db154b1edf1bc3
ReanGD <reangd@yandex.ru>
2020-12-22 01:05:34 +0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,184 +1,25 @@
-// import 'dart:async';
-
 import 'package:flutter/material.dart';
-// import 'package:video_player/video_player.dart';
-
-class FilmScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: Text('Kino player'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-          IconButton(
-            icon: Icon(Icons.sort),
-            tooltip: 'Sort',
-            onPressed: null,
-          ),
-        ],
-      ),
-      body: Center(child: _buildGrid()),
-    );
-  }
-
-  Widget _buildGrid() => GridView.extent(
-      maxCrossAxisExtent: 250,
-      padding: const EdgeInsets.all(4),
-      mainAxisSpacing: 100,
-      crossAxisSpacing: 100,
-      children: _buildGridTileList(100));
-
-  List<Container> _buildGridTileList(int count) => List.generate(
-        count,
-        (i) => Container(
-          child: _buildImageBoxes(i),
-        ),
-      );
-
-  Widget _buildImageBoxes(int index) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-          child: Image.network("https://picsum.photos/250/250/?random"),
-        ),
-        Container(
-          padding: EdgeInsets.all(10),
-          child: Text("Image $index"),
-        )
-      ],
-    );
-  }
-}
+import 'package:flutter/services.dart';
+import 'package:kino_player/view/player/player_screen.dart';
+import 'package:kino_player/view/posters/posters_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Kino player',
-    theme: ThemeData(primarySwatch: Colors.blue),
-    home: FilmScreen(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(
+    Shortcuts(
+      shortcuts: <LogicalKeySet, Intent>{
+        LogicalKeySet(LogicalKeyboardKey.select): ActivateIntent(),
+      },
+      child: MaterialApp(
+        title: 'Kino player',
+        theme: ThemeData(
+          focusColor: Colors.yellow[500],
+          splashColor: Colors.white70,
+          primarySwatch: Colors.blue,
+        ),
+        home: PlayerScreen(),
+        // home: PostersScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
+    ),
+  );
 }
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // Try running your application with "flutter run". You'll see the
-//         // application has a blue toolbar. Then, without quitting the app, try
-//         // changing the primarySwatch below to Colors.green and then invoke
-//         // "hot reload" (press "r" in the console where you ran "flutter run",
-//         // or simply save your changes to "hot reload" in a Flutter IDE).
-//         // Notice that the counter didn't reset back to zero; the application
-//         // is not restarted.
-//         primarySwatch: Colors.blue,
-//       ),
-//       // home: MyHomePage(title: 'Flutter Demo Home Page'),
-//       home: VideoPlayerScreen(),
-//     );
-//   }
-// }
-
-// class VideoPlayerScreen extends StatefulWidget {
-//   VideoPlayerScreen({Key key}) : super(key: key);
-
-//   @override
-//   _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
-// }
-
-// class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-//   VideoPlayerController _controller;
-//   Future<void> _initializeVideoPlayerFuture;
-
-//   @override
-//   void initState() {
-//     // Create and store the VideoPlayerController. The VideoPlayerController
-//     // offers several different constructors to play videos from assets, files,
-//     // or the internet.
-//     _controller = VideoPlayerController.network(
-//       'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
-//     );
-
-//     // Initialize the controller and store the Future for later use.
-//     _initializeVideoPlayerFuture = _controller.initialize();
-
-//     // Use the controller to loop the video.
-//     _controller.setLooping(true);
-
-//     super.initState();
-//   }
-
-//   @override
-//   void dispose() {
-//     // Ensure disposing of the VideoPlayerController to free up resources.
-//     _controller.dispose();
-
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Butterfly Video'),
-//       ),
-//       // Use a FutureBuilder to display a loading spinner while waiting for the
-//       // VideoPlayerController to finish initializing.
-//       body: FutureBuilder(
-//         future: _initializeVideoPlayerFuture,
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.done) {
-//             // If the VideoPlayerController has finished initialization, use
-//             // the data it provides to limit the aspect ratio of the video.
-//             return AspectRatio(
-//               aspectRatio: _controller.value.aspectRatio,
-//               // Use the VideoPlayer widget to display the video.
-//               child: VideoPlayer(_controller),
-//             );
-//           } else {
-//             // If the VideoPlayerController is still initializing, show a
-//             // loading spinner.
-//             return Center(child: CircularProgressIndicator());
-//           }
-//         },
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {
-//           // Wrap the play or pause in a call to `setState`. This ensures the
-//           // correct icon is shown.
-//           setState(() {
-//             // If the video is playing, pause it.
-//             if (_controller.value.isPlaying) {
-//               _controller.pause();
-//             } else {
-//               // If the video is paused, play it.
-//               _controller.play();
-//             }
-//           });
-//         },
-//         // Display the correct icon depending on the state of the player.
-//         child: Icon(
-//           _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-//         ),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
