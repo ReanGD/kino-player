bc60bf6e907e96bf54ce35cd6c72a39aa37e102f
ReanGD <reangd@yandex.ru>
2020-12-23 01:11:22 +0300

    refactoring slider

--- lib/widgets/slider.dart
+++ lib/widgets/slider.dart
@@ -1,176 +1,163 @@
-library seekbar;
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class SeekBar extends StatefulWidget {
+  final double max;
+  final double step;
+  final double value;
+  final double markerValue;
+  final bool autofocus;
+  final ValueChanged<double> onChanged;
+  final ValueChanged<double> onChangeStart;
+  final ValueChanged<double> onChangeEnd;
+  // old
   final double progressWidth;
   final double thumbRadius;
-  final double value;
-  final double secondValue;
   final Color barColor;
   final Color progressColor;
   final Color secondProgressColor;
   final Color thumbColor;
-  final Function onStartTrackingTouch;
-  final ValueChanged<double> onProgressChanged;
-  final Function onStopTrackingTouch;
 
   SeekBar({
     Key key,
+    this.max = 100.0,
+    this.step = 1.0,
+    this.value = 0.0,
+    this.markerValue = 0.0,
+    this.autofocus = false,
+    this.onChanged,
+    this.onChangeStart,
+    this.onChangeEnd,
+    // old
     this.progressWidth = 2.0,
     this.thumbRadius = 7.0,
-    this.value = 0.0,
-    this.secondValue = 0.0,
     this.barColor = const Color(0x73FFFFFF),
     this.progressColor = Colors.white,
     this.secondProgressColor = const Color(0xBBFFFFFF),
     this.thumbColor = Colors.white,
-    this.onStartTrackingTouch,
-    this.onProgressChanged,
-    this.onStopTrackingTouch,
   }) : super(key: key);
 
   @override
-  _SeekBarState createState() {
-    return _SeekBarState();
-  }
+  State<StatefulWidget> createState() => _SeekBarState();
 }
 
 class _SeekBarState extends State<SeekBar> {
-  Offset _touchPoint = Offset.zero;
-
+  bool _focused = false;
   double _value = 0.0;
-  double _secondValue = 0.0;
+  double _markerValue = 0.0;
 
-  bool _touchDown = false;
-
-  _setValue() {
-    _value = _touchPoint.dx / context.size.width;
-  }
-
-  _checkTouchPoint() {
-    if (_touchPoint.dx <= 0) {
-      _touchPoint = Offset(0, _touchPoint.dy);
-    }
-    if (_touchPoint.dx >= context.size.width) {
-      _touchPoint = Offset(context.size.width, _touchPoint.dy);
-    }
+  double _clamp(double value, double min, double max) {
+    return value > max
+        ? max
+        : value < min
+            ? min
+            : value;
   }
 
   @override
   void initState() {
-    _value = widget.value > 1
-        ? 1
-        : widget.value < 0
-            ? 0
-            : widget.value;
-    _secondValue = widget.secondValue > 1
-        ? 1
-        : widget.secondValue < 0
-            ? 0
-            : widget.secondValue;
+    _value = _clamp(widget.value, 0, widget.max);
+    _markerValue = _clamp(widget.markerValue, 0, widget.max);
     super.initState();
   }
 
   @override
   void didUpdateWidget(SeekBar oldWidget) {
-    _value = widget.value > 1
-        ? 1
-        : widget.value < 0
-            ? 0
-            : widget.value;
-    _secondValue = widget.secondValue > 1
-        ? 1
-        : widget.secondValue < 0
-            ? 0
-            : widget.secondValue;
+    _value = _clamp(widget.value, 0, widget.max);
+    _markerValue = _clamp(widget.markerValue, 0, widget.max);
     super.didUpdateWidget(oldWidget);
+  }
+
+  double _getTouchValue(Offset touchPoint) {
+    final dx = _clamp(touchPoint.dx, 0, context.size.width);
+    return dx * widget.max / context.size.width;
+  }
+
+  void _onChangeStart(double value) {
+    if (_value != value || !_focused) {
+      setState(() {
+        _value = value;
+        _focused = true;
+      });
+    }
+    if (widget.onChangeStart != null) {
+      widget.onChangeStart(value);
+    }
+  }
+
+  void _onChanged(double value) {
+    if (_value != value) {
+      setState(() {
+        _value = value;
+      });
+    }
+    if (widget.onChanged != null) {
+      widget.onChanged(value);
+    }
+  }
+
+  void _onChangeEnd() {
+    if (_focused) {
+      setState(() {
+        _focused = false;
+      });
+    }
+    if (widget.onChangeEnd != null) {
+      widget.onChangeEnd(_value);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Focus(
-      autofocus: false,
+      autofocus: widget.autofocus,
       onKey: (FocusNode node, RawKeyEvent event) {
-        double dx = context.size.width / 100.0;
         if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
-          dx = -dx;
-        } else if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
-          dx = dx;
-        } else {
-          return false;
+          _onChanged(_value - widget.step);
+          return true;
         }
-        _touchPoint = Offset(_touchPoint.dy, (_touchPoint.dx + dx));
-        _checkTouchPoint();
-        setState(() {
-          _setValue();
-        });
-        if (widget.onProgressChanged != null) {
-          widget.onProgressChanged(_value);
+        if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+          _onChanged(_value + widget.step);
+          return true;
         }
-
         return false;
       },
       onFocusChange: (bool focus) {
-        setState(() {
-          if (focus) {
-            if (widget.onStartTrackingTouch != null) {
-              widget.onStartTrackingTouch();
-            }
-          } else {
-            if (widget.onStopTrackingTouch != null) {
-              widget.onStopTrackingTouch();
-            }
-          }
-          _touchDown = focus;
-        });
+        if (focus) {
+          _onChangeStart(_value);
+        } else {
+          _onChangeEnd();
+        }
       },
       child: GestureDetector(
         onHorizontalDragDown: (details) {
-          RenderBox box = context.findRenderObject();
-          _touchPoint = box.globalToLocal(details.globalPosition);
-          _checkTouchPoint();
-          setState(() {
-            _setValue();
-            _touchDown = true;
-          });
-          if (widget.onStartTrackingTouch != null) {
-            widget.onStartTrackingTouch();
-          }
+          final RenderBox box = context.findRenderObject();
+          final touchPoint = box.globalToLocal(details.globalPosition);
+          _onChangeStart(_getTouchValue(touchPoint));
         },
         onHorizontalDragUpdate: (details) {
-          RenderBox box = context.findRenderObject();
-          _touchPoint = box.globalToLocal(details.globalPosition);
-          _checkTouchPoint();
-          setState(() {
-            _setValue();
-          });
-          if (widget.onProgressChanged != null) {
-            widget.onProgressChanged(_value);
-          }
+          final RenderBox box = context.findRenderObject();
+          final touchPoint = box.globalToLocal(details.globalPosition);
+          _onChanged(_getTouchValue(touchPoint));
         },
         onHorizontalDragEnd: (details) {
-          setState(() {
-            _touchDown = false;
-          });
-          if (widget.onStopTrackingTouch != null) {
-            widget.onStopTrackingTouch();
-          }
+          _onChangeEnd();
         },
         child: Container(
           constraints: BoxConstraints.expand(height: widget.thumbRadius * 2),
           child: CustomPaint(
             painter: _SeekBarPainter(
+              max: widget.max,
+              value: _value,
+              markerValue: _markerValue,
+              // old
               progressWidth: widget.progressWidth,
               thumbRadius: widget.thumbRadius,
-              value: _value,
-              secondValue: _secondValue,
               barColor: widget.barColor,
               progressColor: widget.progressColor,
               secondProgressColor: widget.secondProgressColor,
               thumbColor: widget.thumbColor,
-              touchDown: _touchDown,
+              focused: _focused,
             ),
           ),
         ),
@@ -180,33 +167,38 @@ class _SeekBarState extends State<SeekBar> {
 }
 
 class _SeekBarPainter extends CustomPainter {
+  final double max;
+  final double value;
+  final double markerValue;
+  // old
   final double progressWidth;
   final double thumbRadius;
-  final double value;
-  final double secondValue;
   final Color barColor;
   final Color progressColor;
   final Color secondProgressColor;
   final Color thumbColor;
-  final bool touchDown;
+  final bool focused;
 
   _SeekBarPainter({
+    this.max,
+    this.value,
+    this.markerValue,
+    // old
     this.progressWidth,
     this.thumbRadius,
-    this.value,
-    this.secondValue,
     this.barColor,
     this.progressColor,
     this.secondProgressColor,
     this.thumbColor,
-    this.touchDown,
+    this.focused,
   });
 
   @override
   bool shouldRepaint(_SeekBarPainter old) {
-    return value != old.value ||
-        secondValue != old.secondValue ||
-        touchDown != old.touchDown;
+    return max != old.max ||
+        value != old.value ||
+        markerValue != old.markerValue ||
+        focused != old.focused;
   }
 
   @override
@@ -222,9 +214,9 @@ class _SeekBarPainter extends CustomPainter {
     final Offset startPoint = Offset(thumbRadius, centerY);
     final Offset endPoint = Offset(size.width - thumbRadius, centerY);
     final Offset progressPoint =
-        Offset(barLength * value + thumbRadius, centerY);
+        Offset(barLength * (value / max) + thumbRadius, centerY);
     final Offset secondProgressPoint =
-        Offset(barLength * secondValue + thumbRadius, centerY);
+        Offset(barLength * (markerValue / max) + thumbRadius, centerY);
 
     paint.color = barColor;
     canvas.drawLine(startPoint, endPoint, paint);
@@ -240,7 +232,7 @@ class _SeekBarPainter extends CustomPainter {
     thumbPaint.color = Colors.transparent;
     canvas.drawCircle(progressPoint, centerY, thumbPaint);
 
-    if (touchDown) {
+    if (focused) {
       thumbPaint.color = thumbColor.withOpacity(0.6);
       canvas.drawCircle(progressPoint, thumbRadius, thumbPaint);
     }
