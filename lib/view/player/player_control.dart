ed7db23827338ac1162e967b41901f10f37bca8b
ReanGD <reangd@yandex.ru>
2020-12-24 03:35:22 +0300

    add control button

---
+++ lib/view/player/player_control.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ControlButton extends StatelessWidget {
+  const ControlButton(this._icon, this._onPressed, {this.autofocus = false});
+
+  final IconData _icon;
+  final VoidCallback _onPressed;
+  final bool autofocus;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      constraints: BoxConstraints(minWidth: 35, minHeight: 35),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      onPressed: _onPressed,
+      autofocus: autofocus,
+      child: Builder(
+        builder: (BuildContext context) {
+          return Icon(_icon,
+              color: Focus.of(context).hasPrimaryFocus
+                  ? Colors.black87
+                  : Colors.white70);
+        },
+      ),
+    );
+  }
+}
