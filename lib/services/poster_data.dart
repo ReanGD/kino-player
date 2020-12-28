b424a15be0705fa2f477737297e4c0ce808d79c2
ReanGD <reangd@yandex.ru>
2020-12-28 20:36:41 +0300

    PostersData with new api

--- lib/services/poster_data.dart
+++ lib/services/poster_data.dart
@@ -1,14 +1,18 @@
+import 'package:kino_player/services/content_data.dart';
+import 'package:kino_player/services/kino_pub_api.dart';
+
 class PosterData {
-  final int id;
+  final int posterId;
   final int year;
   final double imdbRating;
   final double kinopoiskRating;
   final double kinopubRating;
+  final String plot;
   final String title;
   final String poster;
 
   PosterData.fromJson(Map<String, dynamic> json)
-      : id = json["id"],
+      : posterId = json["id"],
         year = json["year"],
         imdbRating =
             json["imdb_rating"] == null ? 0 : json["imdb_rating"].toDouble(),
@@ -18,8 +22,13 @@ class PosterData {
         kinopubRating = json["rating_percentage"] == null
             ? 0
             : json["rating_percentage"].toDouble() / 10.0,
+        plot = json["plot"],
         title = json["title"],
         poster = json["posters"]["small"];
+
+  Future<ContentData> getContent() {
+    return KinoPubApi.instance.getContent(this);
+  }
 }
 
 class PostersData {
