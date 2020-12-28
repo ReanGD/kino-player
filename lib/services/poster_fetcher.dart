d94626fa5189ebf35f5efc81cec747bf28799eb2
ReanGD <reangd@yandex.ru>
2020-12-28 20:27:44 +0300

    add posters fetcher class

--- lib/services/poster_fetcher.dart
+++ lib/services/poster_fetcher.dart
@@ -1,17 +1,20 @@
 import 'package:kino_player/services/poster_data.dart';
-import 'package:kino_player/services/kino_pub_api.dart';
 
-class FreshLoader {
-  final _api = KinoPubApi.instance;
-  static const String _type = "movie";
+typedef AsyncPostersCallback = Future<PostersData> Function(
+    int page, int perPage);
+
+class PosterFetcher {
   static const int _perPage = 25;
+  final AsyncPostersCallback _callback;
   int _page = 0;
   int _total = 0;
+
+  PosterFetcher(this._callback);
 
   int get total => _total;
 
   Future<List<PosterData>> getNext() async {
-    final result = await _api.getFresh(_type, _page, _perPage);
+    final result = await _callback(_page, _perPage);
     _total = result.total;
     _page++;
     return result.posters;
