40d4efec32967fa5cee5014abfdbaead97405362
ReanGD <reangd@yandex.ru>
2021-01-01 20:12:53 +0300

    add styly and localization for main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:kino_player/view/player/player_screen.dart';
 import 'package:kino_player/view/posters/posters_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(
@@ -10,14 +11,20 @@ void main() {
         LogicalKeySet(LogicalKeyboardKey.select): ActivateIntent(),
       },
       child: MaterialApp(
+        localizationsDelegates: [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
         title: 'Kino player',
         theme: ThemeData(
-          focusColor: Colors.yellow[500],
+          focusColor: Colors.yellow[800],
           splashColor: Colors.white70,
           primarySwatch: Colors.blue,
         ),
-        home: PlayerScreen(),
-        // home: PostersScreen(),
+        home: PostersScreen(),
         debugShowCheckedModeBanner: false,
       ),
     ),
